0b9e34135779c6ce92a0f652ea7379ed63395e5e
Sam Fisher <sfisher2@gmail.com>
2015-01-04 16:21:33 +1000

    Created script to insert sample data

---
+++ SQL Scripts/Insert data for match, games, and ELO.sql
@@ -0,0 +1,58 @@
+INSERT INTO match (timestamp, location, software_id, winner_id, winner_ratio) VALUES
+	('2015-01-03 16:48', 'The Bungalow', 1, 1, 0.6);
+
+SET @matchid = SELECT LAST_INSERT_ID();
+
+
+# First game
+
+INSERT INTO game (stage_id, match_id, winner_id) VALUES
+	(48, @matchid, 1);
+
+SET @gameid = SELECT LAST_INSERT_ID();
+
+INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
+	(1, 47, @gameid, (1450 + 4));
+
+INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
+	(2, 40, @gameid, (1450 - 4);
+
+
+
+# Second game
+
+INSERT INTO game (stage_id, match_id, winner_id) VALUES
+	(68, @matchid, 2);
+
+SET @gameid = SELECT LAST_INSERT_ID();
+
+INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
+	(1, 18, @gameid, (1450 - 4));
+
+INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
+	(2, 40, @gameid, (1450 - 4  + 4);
+
+
+
+# Third game
+
+INSERT INTO game (stage_id, match_id, winner_id) VALUES
+	(71, @matchid, 1);
+
+SET @gameid = SELECT LAST_INSERT_ID();
+
+INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
+	(1, 47, @gameid, (1450 + 4) + 4);
+
+INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
+	(2, 40, @gameid, (1450 - 4  + 4 - 4);
+
+
+
+# Player ELO
+
+INSERT INTO player_elo (player_id, match_id, elo_rating) VALUES
+	(1, @matchid, (1582+2));
+
+INSERT INTO player_elo (player_id, match_id, elo_rating) VALUES
+	(2, @matchid, (1515-2));
