7bff81598512f43839dfba6e09c3ec0b3133ce93
Sam Fisher <sfisher2@gmail.com>
2015-01-04 16:44:26 +1000

    Fixed minor bugs (of the typographical error variety)

--- SQL Scripts/Insert data for match, games, and ELO.sql
+++ SQL Scripts/Insert data for match, games, and ELO.sql
@@ -1,7 +1,7 @@
-INSERT INTO match (timestamp, location, software_id, winner_id, winner_ratio) VALUES
-	('2015-01-03 16:48', 'The Bungalow', 1, 1, 0.6);
+INSERT INTO `match` (`timestamp`, location, software_id, winner_id, winner_ratio) VALUES
+	('2015-01-03 16:48:00', 'The Bungalow', 1, 1, 0.6);
 
-SET @matchid = SELECT LAST_INSERT_ID();
+SET @matchid = LAST_INSERT_ID();
 
 
 # First game
@@ -9,13 +9,13 @@ SET @matchid = SELECT LAST_INSERT_ID();
 INSERT INTO game (stage_id, match_id, winner_id) VALUES
 	(48, @matchid, 1);
 
-SET @gameid = SELECT LAST_INSERT_ID();
+SET @gameid = LAST_INSERT_ID();
 
 INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
 	(1, 47, @gameid, (1450 + 4));
 
 INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
-	(2, 40, @gameid, (1450 - 4);
+	(2, 40, @gameid, (1450 - 4));
 
 
 
@@ -24,13 +24,13 @@ INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES
 INSERT INTO game (stage_id, match_id, winner_id) VALUES
 	(68, @matchid, 2);
 
-SET @gameid = SELECT LAST_INSERT_ID();
+SET @gameid = LAST_INSERT_ID();
 
 INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
 	(1, 18, @gameid, (1450 - 4));
 
 INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
-	(2, 40, @gameid, (1450 - 4  + 4);
+	(2, 40, @gameid, (1450 - 4  + 4));
 
 
 
@@ -39,13 +39,13 @@ INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES
 INSERT INTO game (stage_id, match_id, winner_id) VALUES
 	(71, @matchid, 1);
 
-SET @gameid = SELECT LAST_INSERT_ID();
+SET @gameid = LAST_INSERT_ID();
 
 INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
-	(1, 47, @gameid, (1450 + 4) + 4);
+	(1, 47, @gameid, (1450 + 4 + 4));
 
 INSERT INTO player_game (player_id, character_id, game_id, character_elo) VALUES 
-	(2, 40, @gameid, (1450 - 4  + 4 - 4);
+	(2, 40, @gameid, (1450 - 4  + 4 - 4));
 
 
 
